068f8e93be816e4af8d7416f2982b6d3ddf48f61
Matthew <Bean6754@users.noreply.github.com>
2016-12-27 18:01:05 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -88,12 +88,15 @@ else
     exit 1
 fi
 
-sleep 5s
-
 sudo cp -r configs/sources.list /etc/apt/sources.list
 
 clear
 
+echo "Deleting potential apt lock files."
+sudo rm -rf /var/lib/apt/lists/lock
+sudo rm -rf /var/cache/apt/archives/lock
+sudo rm -rf /var/lib/dpkg/lock
+echo "Installing packages using apt (apt-get)."
 sudo apt-get update -y
 sudo apt-get purge -y lubuntu-desktop lightdm file-roller lxterminal firefox abiword gnumeric mplayer
 sudo apt-get install -y aptitude git wget curl vim rxvt-unicode i3 i3lock i3status i3blocks fonts-font-awesome lubuntu-restricted-extras lubuntu-restricted-addons vlc p7zip-full unrar rar build-essential redshift chromium-browser libreoffice gimp xarchiver software-properties-gtk steam transmission-gtk transmission-cli default-jdk qt5-default virtualbox alsa-base alsa-utils pulseaudio pavucontrol libdvd-pkg libbluray1
