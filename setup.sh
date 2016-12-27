12d9224e67bf28a5e55c7af3a1cbd6fd13809d1d
Matthew <Bean6754@users.noreply.github.com>
2016-12-27 17:15:10 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -18,8 +18,22 @@ else
     exit 1
 fi
 
-
 echo "Starting..."
+
+# Check if 'config' directory exists or not.
+if [ ! -d "config" ]; then
+  echo "'config' directory not found. Exiting.." 1>&2
+  exit 1
+else
+  echo "'config' directory found! Continuing.." 1>&2
+fi
+#Check if the files in there exist.
+if [ ! -f "config/.Xdefaults"]; then
+  echo "'.Xdefaults' file was not found. Exiting.." 1>&2
+  exit 1
+else
+  echo "'.Xdefaults' file was found! Continuing.." 1>&2
+fi
 
 
 sudo cp -r configs/sources.list /etc/apt/sources.list
@@ -44,8 +58,15 @@ cp -r ~/.config/i3/* ~/configbackups/i3/
 cp -r ~/.vimrc ~/configbackups/.vimrc
 cp -r ~/.Xdefaults ~/configbackups/.Xdefaults
 cp -r ~/.xinitrc ~/configbackups/.xinitrc
-echo "Deleting i3 config folder."
+echo "Deleting potentially existing config files.
+rm -rf ~/.Xdefaults
+rm -rf ~/.bash_profile
 rm -rf ~/.config/i3/
+rm -rf ~/.vimrc
+sudo rm -rf /root/.vimrc
+rm -rf ~/.xinitrc
+echo "Copying '.Xdefaults' rxvt-unicode config file to '~/.Xdefaults'"
+cp -r configs/.Xdefaults ~/.Xdefaults
 echo "Copying '.bash_profile' bash login config to '~/.bash_profile'"
 cp -r configs/.bash_profile ~/.bash_profile
 echo "Copying i3 config files to '~/.config/i3/'"
@@ -54,8 +75,6 @@ cp -r configs/i3/* ~/.config/i3/
 echo "Copying '.vimrc' vim config file to '~/.vimrc' and to '/root/.vimrc'"
 cp -r configs/.vimrc ~/.vimrc
 sudo cp -r configs/.vimrc /root/.vimrc
-echo "Copying '.Xdefaults' rxvt-unicode config file to '~/.Xdefaults'"
-cp -r configs/.Xdefaults ~/.Xdefaults
 echo "Copying '.xinitrc' xorg config file to '~/.xinitrc'"
 cp -r configs/.xinitrc ~/.xinitrc
 
