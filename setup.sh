91e220465f2d64f093f2d160a5e0238be0d16244
Matthew <Bean6754@users.noreply.github.com>
2016-12-27 20:03:45 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -110,7 +110,7 @@ sudo rm -rf /var/cache/apt/archives/lock
 sudo rm -rf /var/lib/dpkg/lock
 echo "Installing packages using apt (apt-get)."
 sudo apt-get update -y
-sudo apt-get install -y aptitude git wget curl vim links rxvt-unicode xorg xinit i3 i3lock i3status i3blocks suckless-tools feh rofi fonts-font-awesome ubuntu-restricted-extras ubuntu-restricted-addons lxappearance vlc p7zip-full unrar rar build-essential redshift chromium-browser libreoffice gimp xarchiver software-properties-gtk steam transmission-gtk transmission-cli default-jdk qt5-default virtualbox alsa-base alsa-utils pulseaudio pavucontrol libdvd-pkg libbluray1
+sudo apt-get install -y aptitude git wget curl vim links rxvt-unicode xorg xinit i3 i3lock i3status i3blocks suckless-tools feh rofi fonts-liberation fonts-dejavu fonts-ubuntu-font-family-console ttf-ubuntu-font-family fonts-font-awesome ubuntu-restricted-extras ubuntu-restricted-addons lxappearance vlc p7zip-full unrar rar build-essential redshift chromium-browser libreoffice gimp xarchiver software-properties-gtk steam transmission-gtk transmission-cli default-jdk qt5-default virtualbox alsa-base alsa-utils pulseaudio pavucontrol libdvd-pkg libbluray1
 sudo dpkg-reconfigure libdvd-pkg
 sudo apt-get autoremove -y
 
@@ -156,6 +156,9 @@ cp -r configs/.xinitrc ~/.xinitrc
 ## Wallpaper link: https://pixabay.com/en/wave-atlantic-pacific-ocean-huge-1913559/
 mkdir -p ~/Pictures/
 cp -r Pictures/wave-1913559.jpg ~/Pictures/Wallpaper.jpg
+
+echo "Create user directories."
+mkdir -p ~/Desktop/ ~/Documents/ ~/Downloads/ ~/Music/ ~/Public/ ~/Templates/ ~/Videos/ ~/.fonts ~/.icons ~/.themes
 
 echo "All done!"
 
